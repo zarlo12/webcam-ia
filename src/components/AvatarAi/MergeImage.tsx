987b217d2e2ed29f630b1b4ec3c9c82c61c3c2b4
Marco Salazar <marco@honeymustard.mx>
2025-06-26 12:31:35 -0500

    feat: mejorar espacio imagen logo inferior

--- src/components/AvatarAi/MergeImage.tsx
+++ src/components/AvatarAi/MergeImage.tsx
@@ -45,7 +45,7 @@ const MergeImage: React.FC<MergeImageProps> = ({
         ctx.drawImage(avatar, 0, 0, canvas.width, canvas.height);
 
         // Factor de escala y margen para el logo inferior
-        const scale = 0.632;
+        const scale = 0.633;
         const margin = 0;
 
         //const extraTopOffset =  margin*3; // distancia adicional para el logo superior

a3ffd6c6c24ceac0cefb8d56e133dfc24b68d716
Marco Salazar <marco@honeymustard.mx>
2025-10-22 17:55:10 -0500

    test

--- src/components/AvatarAi/MergeImage.tsx
+++ src/components/AvatarAi/MergeImage.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import fondo from '../../assets/electrolux/fondo.png'
+import fondo from '../../assets/img/TextIZQsup.png'
 
 interface MergeImageProps {
   imageUrl: string; // URL de la imagen principal (avatar)

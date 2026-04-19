f43561bac2b78f5f6593729409f22699fe1bf31c
Marco Salazar <marco@honeymustard.mx>
2026-04-19 16:33:51 -0500

    test23

--- src/components/AvatarAi/AvatarPhoto.tsx
+++ src/components/AvatarAi/AvatarPhoto.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import "./AvatarPhoto.scss";
-import logo from "../../assets/img/logo_final.png";
+
 import WebcamScene from "../WebcamScene";
 import aiImageService from "../../services/aiImageService";
 import Swal from "sweetalert2";
